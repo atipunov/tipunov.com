036581bbb8387e2fe0e66418b105b0569437f4c6
Anton Tipunov <a.tipunov@revotechs.com>
2015-09-19 12:00:29 +0300

    inject GA only for production

---
+++ app/content/scripts/directives/social.js
@@ -0,0 +1,13 @@
+tipunovApp.directive('social', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        template: '<li class="piece {{social.className}}">\
+                        <a class="link" href="{{social.url}}" rel="me">\
+                            <div class="icon"></div>\
+                            <span class="service">{{social.label}}</span><span class="dash">/</span>\
+                            <span class="login">{{social.login}}</span>\
+                        </a>\
+                    </li>'
+    };
+});
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gutil = require('gulp-util');
 var gulpIf = require('gulp-if');
 var minifyCSS = require('gulp-minify-css');
 var del = require('del');
+var inject = require('gulp-inject');
 
 gulp.task('sass', function() {
     return gulp.src('app/content/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
@@ -23,11 +24,18 @@ gulp.task('build', ['clean', 'sass'], function(){
     var assets = useref.assets();
 
     return gulp.src('app/*.html')
+        .pipe(inject(gulp.src(['./app/content/templates/*.html']), {
+            starttag: '<!-- inject:ga -->',
+            transform: function (filePath, file) {
+                // return file contents as string
+                return file.contents.toString('utf8')
+            }
+        }))
         .pipe(assets)
         .pipe(gulpIf('*.css', minifyCSS()))
         .pipe(gulpIf('*.js', uglify().on('error', gutil.log)))
         .pipe(assets.restore())
         .pipe(useref())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist'));
 });
 
